a0accfd5577fc0efcc3a52be7459e7f84917b5c9
Daniel Korsah <dkorsah15@aol.co.uk>
2016-11-18 14:30:40 +0000

    Added accessord for customer class

--- Coursework-2/Customer.cs
+++ Coursework-2/Customer.cs
@@ -14,5 +14,27 @@ namespace Coursework_2
         private string name; //name of person making booking
         private string address; //address of said customer
         private int customerRef; //unique identifying number for each guest
+
+        public string Name //name accessor
+        {
+            get { return name; } //allow fetching of current value of name
+            set { name = value; } //allow name to be set to a new value when necessary
+        }
+
+        public string Address //address accessor - does the same as above but for address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public int CustomerRef //customer ref accessor - does the same as name accessor and address accessor but for customerRef
+        {
+            get { return customerRef; }
+            set { customerRef = value; }
+        }
+
+
+
+
     }
 }
