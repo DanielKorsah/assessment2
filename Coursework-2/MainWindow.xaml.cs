ec8d4f32a74229f62f4d343839f04fd4879d44e6
Daniel Korsah <dkorsah15@aol.co.uk>
2016-11-18 17:03:26 +0000

    New and returnng customer buttons added.

--- Coursework-2/MainWindow.xaml.cs
+++ Coursework-2/MainWindow.xaml.cs
@@ -23,9 +23,18 @@ namespace Coursework_2
         public MainWindow()
         {
             InitializeComponent();
-
+            newButton.Content = "new &#xa Customer";
            
         }
-        
+
+        private void newButton_Click(object sender, RoutedEventArgs e) //execute when New Customer button is clicked on GUI
+        {
+
+        }
+
+        private void returningButton_Click(object sender, RoutedEventArgs e) //execute when Returning Customer button is clicked on GUI
+        {
+
+        }
     }
 }
